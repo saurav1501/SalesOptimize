79a9a96a64a8f357b9089530b3089e243e81ac32
Saurav Sinha <sauravsinha@Sauravs-MacBook-Air.local>
2021-03-18 20:00:05 +0530

    Test

--- src/test/java/com/salesoptimize/reusablemethods/ReusableMethodsDashboard.java
+++ src/test/java/com/salesoptimize/reusablemethods/ReusableMethodsDashboard.java
@@ -1,127 +1,122 @@
 package com.salesoptimize.reusablemethods;
 
-import java.awt.Robot;
-import java.util.List;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+
 import com.salesoptimize.utility.BaseClass;
 import com.salesoptimize.utility.CommonMethod;
-import com.sun.glass.events.KeyEvent;
 
-public class ReusableMethodsDashboard extends BaseClass{
-	
+public class ReusableMethodsDashboard extends BaseClass {
+
 	public void selectBusinessCountry(int rowNum, String country) throws Exception {
-	
-		CommonMethod.testlog("Pass","Test Starated");
+
+		CommonMethod.testlog("Pass", "Test Starated");
 		CommonMethod.scrolldowntoElement("businessdropdown");
-		CommonMethod.elementToBeClickable("businessdropdown", minWait);
 		CommonMethod.click("businessdropdown");
-		
-		CommonMethod.scrolldowntoElement("businessCountryPuls");
-		Thread.sleep(2000);
-		CommonMethod.elementToBeClickable("businessCountryPuls", minWait);
-		
+
+		CommonMethod.moveToElement("businessCountryPuls");
 		CommonMethod.click("businessCountryPuls");
-		Thread.sleep(1000);
-		
-		Robot robot = new Robot();
-		robot.keyPress(KeyEvent.VK_ENTER);
-		
-		CommonMethod.sendKeys("businesscountry",data.getCellData(country,"BusinessLocation",rowNum));
-		Thread.sleep(2000);
-		
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		Thread.sleep(1000);
-		
-		Assert.assertEquals(CommonMethod.getText("BusinessCountryText"), data.getCellData(country,"BusinessLocation",rowNum),"Success");
-		CommonMethod.testlog("Pass","Test Passed");
+
+		CommonMethod.sendKeys("businesscountry", data.getCellData(country, "BusinessLocation", rowNum));
+
+		CommonMethod.visibilityOf("FirstResult", minWait);
+		CommonMethod.click("FirstResult");
+
+		Assert.assertEquals(CommonMethod.getText("BusinessCountryText"),
+				data.getCellData(country, "BusinessLocation", rowNum), "Success");
+		CommonMethod.testlog("Pass", "Test Passed");
 	}
-	
-	
+
 	public void selectIndustryRetailAndFashiClothing(int rowNum, String country) throws Exception {
-		CommonMethod.testlog("Pass","Test Starated");
-		CommonMethod.elementToBeClickable("industryDropdown", minWait);
+		CommonMethod.testlog("Pass", "Test Starated");
+
+		CommonMethod.moveToElement("industryDropdown");
 		CommonMethod.click("industryDropdown");
-		
-		CommonMethod.elementToBeClickable("IndustoryPlus", minWait);
+
+		CommonMethod.moveToElement("IndustoryPlus");
 		CommonMethod.click("IndustoryPlus");
-		Thread.sleep(1000);
-		Robot robot = new Robot();
-		robot.keyPress(KeyEvent.VK_ENTER);
-		CommonMethod.sendKeys("Selectindustry",data.getCellData(country,"Industry",rowNum));
-		Thread.sleep(2000);
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		Thread.sleep(1000);
-		Assert.assertEquals(CommonMethod.getText("Industorychosen"), data.getCellData(country,"Industry",rowNum),"Success");
-		
+
+		CommonMethod.moveToElement("Selectindustry");
+		CommonMethod.sendKeys("Selectindustry", data.getCellData(country, "Industry", rowNum));
+
+		CommonMethod.visibilityOf("FirstResult", minWait);
+		CommonMethod.click("FirstResult");
+
+		Assert.assertEquals(CommonMethod.getText("Industorychosen"), data.getCellData(country, "Industry", rowNum),
+				"Success");
+
+		CommonMethod.visibilityOf("SubIndustoryPlus", minWait);
 		CommonMethod.elementToBeClickable("SubIndustoryPlus", minWait);
 		CommonMethod.click("SubIndustoryPlus");
-		Thread.sleep(1000);
-		robot.keyPress(KeyEvent.VK_ENTER);
-		CommonMethod.sendKeys("GlobalRegion",data.getCellData(country,"Industry",3));
-		Thread.sleep(2000);
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		Thread.sleep(1000);
-		Assert.assertEquals(CommonMethod.getText("SubIndustory"), data.getCellData(country,"Industry",3),"Success");
-		CommonMethod.testlog("Pass","Test Passed");
-		
-		
+
+		CommonMethod.visibilityOf("GlobalRegion", minWait);
+		CommonMethod.sendKeys("GlobalRegion", data.getCellData(country, "Industry", 3));
+
+		CommonMethod.visibilityOf("FirstResult", minWait);
+		CommonMethod.click("FirstResult");
+
+		Assert.assertEquals(CommonMethod.getText("SubIndustory"), data.getCellData(country, "Industry", 3), "Success");
+		CommonMethod.testlog("Pass", "Test Passed");
+
 	}
-	
-public void selectCompanyTurnover1AndAbove(int rowNum, String country) throws Exception {
-	    CommonMethod.testlog("Pass","Test Starated");
-	    CommonMethod.elementToBeClickable("merchantFilter", minWait);
+
+	public void selectCompanyTurnover1AndAbove(int rowNum, String country) throws Exception {
+		CommonMethod.testlog("Pass", "Test Starated");
+		CommonMethod.elementToBeClickable("merchantFilter", minWait);
 		CommonMethod.click("merchantFilter");
 		CommonMethod.scrolldowntoElement("companyTurnoverSliderStart");
 		Thread.sleep(2000);
-		CommonMethod.dragAndDrop("companyTurnoverSliderStart","companyTurnoverSliderEnd");
+		CommonMethod.dragAndDrop("companyTurnoverSliderStart", "companyTurnoverSliderEnd");
 		CommonMethod.testlog("Pass", "Able to select Company Turnover as 1M and above");
-		CommonMethod.testlog("Pass","Test Passed");
-		
-	}
-	
-public void searchButtonAndCaptureResult(int rowNum, String searchresult) throws Exception {
-	CommonMethod.testlog("Pass","Test Starated");
-	CommonMethod.elementToBeClickable("searchButton", minWait);
-	CommonMethod.click("searchButton");
-	CommonMethod.visibilityOf("numberOfResult", minWait);
-	Assert.assertEquals(CommonMethod.getText("numberOfResult").contains(data.getCellData(searchresult, "numberOfResult", rowNum)),true);
-	CommonMethod.testlog("Pass","Test Passed");
-}
+		CommonMethod.testlog("Pass", "Test Passed");
 
-public void firstRecordCaptureDetail(int rowNum, String country) throws Exception {
-	CommonMethod.testlog("Pass","Test Starated");
-    CommonMethod.elementToBeClickable("info", minWait);
-	CommonMethod.click("info");
-	Thread.sleep(3000);
-    CommonMethod.switchToNewTab();
-	Thread.sleep(2000);
-    CommonMethod.Isdisplayed("newTabValue", minWait);
-     
-	for(int i=2;i<=11;i++) {
-		if(i==9) {
-			System.out.println("Extra Space");
-		}
-		else if(i==10 || i==11) {
-			int j= i;
-			j = j-1;
-			data.setCellData(country, "CompanyDetails", j, driver.findElement(By.xpath("//[@id='so-vue-app']/div/div[1]/div[1]/div/div/div/table/tbody/tr["+i+"]/td[1]")).getText());
-			data.setCellData(country, "Values", j, driver.findElement(By.xpath("//[@id='so-vue-app']/div/div[1]/div[1]/div/div/div/table/tbody/tr["+i+"]/td[2]")).getText());
-			
-		}
-		else {
-		System.out.println(data.setCellData(country, "CompanyDetails",i, driver.findElement(By.xpath("//[@id='so-vue-app']/div/div[1]/div[1]/div/div/div/table/tbody/tr["+i+"]/td[1]")).getText()));
-		System.out.println(data.setCellData(country, "Values", i, driver.findElement(By.xpath("//[@id='so-vue-app']/div/div[1]/div[1]/div/div/div/table/tbody/tr["+i+"]/td[2]")).getText()));
-		
 	}
+
+	public void searchButtonAndCaptureResult(int rowNum, String searchresult) throws Exception {
+		CommonMethod.testlog("Pass", "Test Starated");
+		CommonMethod.elementToBeClickable("searchButton", minWait);
+		CommonMethod.click("searchButton");
+		CommonMethod.visibilityOf("numberOfResult", minWait);
+		String data = CommonMethod.getText("numberOfResult");
+		Assert.assertEquals(data,"163 results found");
+		CommonMethod.testlog("Pass", "Test Passed");
 	}
-	CommonMethod.testlog("Pass","Captured All table values");
-	CommonMethod.testlog("Pass","Test Passed");
-}
+
+	public void firstRecordCaptureDetail(int rowNum, String country) throws Exception {
+		CommonMethod.testlog("Pass", "Test Starated");
+		CommonMethod.elementToBeClickable("info", minWait);
+		CommonMethod.click("info");
+		Thread.sleep(3000);
+		CommonMethod.switchToNewTab();
+		Thread.sleep(2000);
+		CommonMethod.Isdisplayed("newTabValue", minWait);
+
+		for (int i = 2; i <= 11; i++) {
+			if (i == 9) {
+				System.out.println("Extra Space");
+			} else if (i == 10 || i == 11) {
+				int j = i;
+				j = j - 1;
+
+				data.setCellData(country, "CompanyDetails", j,
+						driver.findElement(By.xpath("(//table/tbody/tr[" + i + "]/td[1])[1]")).getText());
+				data.setCellData(country, "Values", j,
+						driver.findElement(By.xpath("(//table/tbody/tr[" + i + "]/td[2])[1]")).getText());
+
+			} else {
+				System.out.println(data.setCellData(country, "CompanyDetails", i,
+						driver.findElement(By.xpath(
+								"(//table/tbody/tr[" + i + "]/td[1])[1]"))
+								.getText()));
+				System.out.println(data.setCellData(country, "Values", i,
+						driver.findElement(By.xpath(
+								"(//table/tbody/tr["+i+"]/td[2])[1]"))
+								.getText()));
+
+			}
+		}
+		CommonMethod.testlog("Pass", "Captured All table values");
+		CommonMethod.testlog("Pass", "Test Passed");
+	}
 
 }
--- src/test/java/com/salesoptimize/utility/BaseClass.java
+++ src/test/java/com/salesoptimize/utility/BaseClass.java
@@ -1,4 +1,5 @@
 package com.salesoptimize.utility;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -34,7 +35,7 @@ import com.salesoptimize.reusablemethods.ReusableMethodsNavigation;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
-	public static Logger log; 	
+	public static Logger log;
 	public static WebDriver driver;
 	public static XlsReader data;
 	public static Properties prop;
@@ -46,176 +47,171 @@ public class BaseClass {
 	public static String TestCaseName;
 	public static ThreadLocal<ExtentTest> testlog;
 	public static int rownumber = 2;
-	public static File extentconfigfile = new File(System.getProperty("user.dir") + "/ObjectRepository/extent-config.xml");
+	public static File extentconfigfile = new File(
+			System.getProperty("user.dir") + "/ObjectRepository/extent-config.xml");
 	public static String downloadPath = System.getProperty("user.dir") + File.separator + "Downloads" + File.separator;
 	public static String baseURL;
 	public static String username;
 	public static String password;
 	public static String SuiteName;
 	public static WebDriverWait wait;
-	public static int minWait=15;
-	public static int avgWait=30;
-	public static int maxWait=60;
+	public static int minWait = 15;
+	public static int avgWait = 30;
+	public static int maxWait = 60;
 	public static String environment;
 	public static String browserName;
 	public static ReusableMethodsNavigation navigation = new ReusableMethodsNavigation();
 	public static ReusableMethodsLogin login = new ReusableMethodsLogin();
 	public static ReusableMethodsDashboard dashboard = new ReusableMethodsDashboard();
-	
+
 	@BeforeSuite
 	public void setup() throws Exception {
 		data = new XlsReader(System.getProperty("user.dir") + "/TestData/SalesTest.xlsx");
 		prop = new Properties();
 
 		System.out.println(System.getenv("agentName"));
-			
-		FileInputStream confi = new FileInputStream(System.getProperty("user.dir")+"/Env/Config.properties");
+
+		FileInputStream confi = new FileInputStream(System.getProperty("user.dir") + "/Env/Config.properties");
 		prop.load(confi);
-		
-		if((System.getenv("browserName")!=null && !System.getenv("browserName").isEmpty())
-				
-		 && System.getenv("environment")!=null && !System.getenv("environment").isEmpty() ){
-			
+
+		if ((System.getenv("browserName") != null && !System.getenv("browserName").isEmpty())
+
+				&& System.getenv("environment") != null && !System.getenv("environment").isEmpty()) {
+
 			browserName = System.getenv("browserName");
-			environment   =   System.getenv("environment");
-			
+			environment = System.getenv("environment");
+
 			System.out.println(browserName);
 			System.out.println(environment);
 			System.out.println(System.getProperty("os.name"));
-			
 
-				
-		}else{
-			
+		} else {
+
 			browserName = prop.getProperty("browserName");
 			environment = prop.getProperty("environment");
-			
+
 			System.out.println(browserName);
 			System.out.println(environment);
 			System.out.println(System.getProperty("os.name"));
-			
+
 		}
-		
-	    	
 
 		if (browserName.equals("firefox")) {
 
 			WebDriverManager.firefoxdriver().setup();
-			System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,"true");
-			System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
-			
-			   FirefoxProfile profile = new FirefoxProfile();
-			
-			   profile.setPreference("browser.download.folderList", 2);
-			   profile.setPreference("browser.download.dir", System.getProperty("user.dir")  + File.separator +"Downloads" + File.separator );
-			   profile.setPreference("browser.download.downloadDir", downloadPath); 
-			   profile.setPreference("browser.download.defaultFolder", downloadPath);
-			   profile.setPreference("browser.download.manager.alertOnEXEOpen", false);
-			   profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/msword, application/csv, application/ris, text/csv, image/png, application/pdf, text/html, text/plain, application/zip, application/x-zip, application/x-zip-compressed, application/download, application/octet-stream, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/excel");
-			   profile.setPreference("browser.download.manager.showWhenStarting", false);
-			   profile.setPreference("browser.download.manager.focusWhenStarting", false);  
-			   profile.setPreference("browser.download.useDownloadDir", true);
-			   profile.setPreference("browser.helperApps.alwaysAsk.force", false);
-			   profile.setPreference("browser.download.manager.alertOnEXEOpen", false);
-			   profile.setPreference("browser.download.manager.closeWhenDone", true);
-			   profile.setPreference("browser.download.manager.showAlertOnComplete", false);
-			   profile.setPreference("browser.download.manager.useWindow", false);
-			   profile.setPreference("services.sync.prefs.sync.browser.download.manager.showWhenStarting", false);
-			   profile.setPreference("pdfjs.disabled", true);
-		
-			   
-		    FirefoxOptions option = new FirefoxOptions();
-		    option.setProfile(profile);
-			/*option.addArguments("-headless");*/
+			System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+			System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
+
+			FirefoxProfile profile = new FirefoxProfile();
+
+			profile.setPreference("browser.download.folderList", 2);
+			profile.setPreference("browser.download.dir",
+					System.getProperty("user.dir") + File.separator + "Downloads" + File.separator);
+			profile.setPreference("browser.download.downloadDir", downloadPath);
+			profile.setPreference("browser.download.defaultFolder", downloadPath);
+			profile.setPreference("browser.download.manager.alertOnEXEOpen", false);
+			profile.setPreference("browser.helperApps.neverAsk.saveToDisk",
+					"application/msword, application/csv, application/ris, text/csv, image/png, application/pdf, text/html, text/plain, application/zip, application/x-zip, application/x-zip-compressed, application/download, application/octet-stream, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/excel");
+			profile.setPreference("browser.download.manager.showWhenStarting", false);
+			profile.setPreference("browser.download.manager.focusWhenStarting", false);
+			profile.setPreference("browser.download.useDownloadDir", true);
+			profile.setPreference("browser.helperApps.alwaysAsk.force", false);
+			profile.setPreference("browser.download.manager.alertOnEXEOpen", false);
+			profile.setPreference("browser.download.manager.closeWhenDone", true);
+			profile.setPreference("browser.download.manager.showAlertOnComplete", false);
+			profile.setPreference("browser.download.manager.useWindow", false);
+			profile.setPreference("services.sync.prefs.sync.browser.download.manager.showWhenStarting", false);
+			profile.setPreference("pdfjs.disabled", true);
+
+			FirefoxOptions option = new FirefoxOptions();
+			option.setProfile(profile);
+			/* option.addArguments("-headless"); */
 			driver = new FirefoxDriver();
-			
-		
+
 		} else if (browserName.equals("chrome")) {
-			 WebDriverManager.chromedriver().setup();
-			// System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/DriverFiles/chromedriver.exe");
-			 ChromeOptions options = new ChromeOptions();
-            /* options.addArguments("headless");
-             options.addArguments("window-size=1200x600");
-		*/	 driver = new ChromeDriver();
-			
+			WebDriverManager.chromedriver().setup();
+			// System.setProperty("webdriver.chrome.driver",
+			// System.getProperty("user.dir")+"/DriverFiles/chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			/*
+			 * options.addArguments("headless");
+			 * options.addArguments("window-size=1200x600");
+			 */ driver = new ChromeDriver();
+
 		}
 
 		driver.manage().window().maximize();
-		//driver.manage().window().setSize(new org.openqa.selenium.Dimension(1366,1280));
+		// driver.manage().window().setSize(new
+		// org.openqa.selenium.Dimension(1366,1280));
 		driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 		driver.manage().timeouts().setScriptTimeout(60, TimeUnit.SECONDS);
 		wait = new WebDriverWait(driver, 30);
-			
-		FileInputStream file1 = new FileInputStream(
-				System.getProperty("user.dir") + "/Env/Stg.properties");
-		FileInputStream file2 = new FileInputStream(
-				System.getProperty("user.dir") + "/Env/Qas.properties");
-		
 
-		if(environment.equalsIgnoreCase("stg")) {
+		FileInputStream file1 = new FileInputStream(System.getProperty("user.dir") + "/Env/Stg.properties");
+		FileInputStream file2 = new FileInputStream(System.getProperty("user.dir") + "/Env/Qas.properties");
+
+		if (environment.equalsIgnoreCase("stg")) {
 			prop.load(file1);
 			baseURL = prop.getProperty("env");
 			driver.get(baseURL);
-			username=prop.getProperty("userName");
-			password=prop.getProperty("Password");
-			/*System.out.println(baseURL);
-			System.out.println(username);
-			System.out.println(password);
-		*/
-		
-		}
-		else if(environment.equalsIgnoreCase("qas")) {
+			username = prop.getProperty("userName");
+			password = prop.getProperty("Password");
+			/*
+			 * System.out.println(baseURL); System.out.println(username);
+			 * System.out.println(password);
+			 */
+
+		} else if (environment.equalsIgnoreCase("qas")) {
 			prop.load(file2);
 			baseURL = prop.getProperty("env");
 			driver.get(baseURL);
-			username=prop.getProperty("userName");
-			password=prop.getProperty("Password");
-			/*System.out.println(baseURL);
-			System.out.println(username);
-			System.out.println(password);
-		
-*/
+			username = prop.getProperty("userName");
+			password = prop.getProperty("Password");
+			/*
+			 * System.out.println(baseURL); System.out.println(username);
+			 * System.out.println(password);
+			 * 
+			 */
 		}
-	    		
+
 		Thread.sleep(3000);
 		Object className = this.getClass().getName();
 		log = LogManager.getLogger((String) className);
 
 	}
 
-    	@BeforeMethod
-    	public static ExtentReports ExtentReportConfig(ITestContext context) throws IOException {
-    		if (extent == null) {
-    			SuiteName = context.getCurrentXmlTest().getSuite().getName();
-    			htmlReporter = new ExtentHtmlReporter(
-    			System.getProperty("user.dir") + "/Reports/SalesOptimize" + SuiteName + ".html");
-    			extent = new ExtentReports();
-    			extent.attachReporter(htmlReporter);
-    			htmlReporter.setAppendExisting(true);
-    			Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();	
-    			extent.setSystemInfo("Host Name", caps.getPlatform().toString());
-    			extent.setSystemInfo("OS", System.getProperty("os.name"));
-    			extent.setSystemInfo("Environment", environment.toUpperCase());
-    			extent.setSystemInfo("BrowserName", caps.getBrowserName()+caps.getVersion());
-    			extent.setSystemInfo("User Name", "Saurav Sinha");
-    			extent.setAnalysisStrategy(AnalysisStrategy.TEST);
-    			htmlReporter.config().setChartVisibilityOnOpen(false);
-    			htmlReporter.config().setDocumentTitle("AutomationTesting Report");
-    			htmlReporter.config().setReportName("ExtentReports");
-    			htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
-    			htmlReporter.config().setTheme(Theme.DARK);
-    			htmlReporter.config().setTheme(Theme.STANDARD);
-    			htmlReporter.loadXMLConfig(extentconfigfile);
-    		}
-    		return extent;
-    	}
-    	
+	@BeforeMethod
+	public static ExtentReports ExtentReportConfig(ITestContext context) throws IOException {
+		if (extent == null) {
+			SuiteName = context.getCurrentXmlTest().getSuite().getName();
+			htmlReporter = new ExtentHtmlReporter(
+					System.getProperty("user.dir") + "/Reports/SalesOptimize" + SuiteName + ".html");
+			extent = new ExtentReports();
+			extent.attachReporter(htmlReporter);
+			htmlReporter.setAppendExisting(true);
+			Capabilities caps = ((RemoteWebDriver) driver).getCapabilities();
+			extent.setSystemInfo("Host Name", caps.getPlatform().toString());
+			extent.setSystemInfo("OS", System.getProperty("os.name"));
+			extent.setSystemInfo("Environment", environment.toUpperCase());
+			extent.setSystemInfo("BrowserName", caps.getBrowserName() + caps.getVersion());
+			extent.setSystemInfo("User Name", "Saurav Sinha");
+			extent.setAnalysisStrategy(AnalysisStrategy.TEST);
+			htmlReporter.config().setChartVisibilityOnOpen(false);
+			htmlReporter.config().setDocumentTitle("AutomationTesting Report");
+			htmlReporter.config().setReportName("ExtentReports");
+			htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+			htmlReporter.config().setTheme(Theme.DARK);
+			htmlReporter.config().setTheme(Theme.STANDARD);
+			htmlReporter.loadXMLConfig(extentconfigfile);
+		}
+		return extent;
+	}
 
 	@AfterSuite(alwaysRun = true)
 	public void end() {
 		driver.quit();
 		System.out.println("EndSuite");
 	}
-	
+
 }
